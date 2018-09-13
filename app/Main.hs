b0f07f0387dd6cd034c918ebba1807fc2cc129d0
ringo-apo <ringo1625@gmail.com>
2018-09-13 12:25:20 +0900

    201809131230

---
+++ app/Main.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Lib
+import Control.Exception
+import Database.MySQL.Base
+import qualified System.IO.Streams as Streams
+import Data.Text.Internal (Text(..), safe, text)
+
+transactional :: MySQLConn -> IO a -> IO a
+transactional conn procedure = mask $ \restore -> do
+  execute_ conn "BEGIN"
+  a <- restore procedure `onException` (execute_ conn "ROLLBACK")
+  execute_ conn "COMMIT"
+  pure a
+
+main :: IO ()
+main = do
+     -- let comment = "apple"
+     comment2 <- getLine
+     let comment = read ("\"" ++ comment2  ++ "\"") :: Data.Text.Internal.Text
+     conn <- connect
+        defaultConnectInfo {ciUser = "root", ciPassword = "password", ciDatabase = "test"}
+     stmt <- prepareStmt conn "INSERT INTO memos (comment) VALUES (?)"
+     transactional conn $ do
+       executeStmt conn stmt [MySQLText comment]
+       (defs, is) <- query_ conn "SELECT * FROM memos"
+       mapM_ print =<< Streams.toList is
